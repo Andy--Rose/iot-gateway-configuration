2a59baa8c1a19803682ef7681b36bb3c19a81cd0
Andy Rose <arose@medallia.com>
2016-11-09 16:58:57 -0700

    bugs

--- Bootstrap/bootstrap-iot.sh
+++ Bootstrap/bootstrap-iot.sh
@@ -4,13 +4,13 @@ echo "Be sure that you have already configured the AWS CLI with credentials!"
 
 bucket="arose-iot-device-certs"
 certDir="~/deviceSDK/certs"
-certs[0]="gateway-private.pem.key"
-certs[1]="gateway-public.pem.key"
-certs[2]="gateway.pem.crt"
-certs[3]="root-CA.pem.crt"
+certpaths[0]="gateway-private.pem.key"
+certpaths[1]="gateway-public.pem.key"
+certpaths[2]="gateway.pem.crt"
+certpaths[3]="root-CA.pem.crt"
 
 mkdir -p $certDir
 
-for cert in certs; do
+for cert in ${certpaths[*]}; do
 	aws s3 cp s3://$bucket/$cert $certDir
 done
